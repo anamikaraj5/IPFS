6d2283447ea769d4b1b69302c9dbe0992f29c898
anamikaraj5 <anamikarajk561@gmail.com>
2025-05-26 11:50:20 +0530

    commit

--- api/[id].js
+++ api/[id].js
@@ -2,5 +2,5 @@ const MetaData = require("../metadata.json")
 
 module.exports=(req,res)=>{
     const {id} = req.query
-    res.json(JSON.parse(JSON.stringify([id])))
+    res.json(JSON.parse(JSON.stringify(MetaData[id])))
 }
